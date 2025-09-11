b0b2132ec1ea1fe410ce78daed59e48d06ed3a0e
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-09-10 23:01:32 -0500

    chore: bootstrap Spring Boot backend + hello endpoint

---
+++ src/main/java/com/resismart/backend/ResiSmartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resismart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiSmartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiSmartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resismart/backend/ResiSmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resismart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiSmartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
